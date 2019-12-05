570349f841e9d60ba3183e58fb451445d74dcab1
Nathan Augusto <48655055+naaaathan@users.noreply.github.com>
2019-12-04 22:21:50 -0300

    Testes na main

--- main.c
+++ main.c
@@ -1,38 +1,21 @@
 #include<stdio.h>
 #include<math.h>
 #include"btree.h"
-#define M 4
-
-typedef struct ArvB {
-        int    qtd;
-        int  chaves[M - 1];
-        struct ArvB *p[M];
-} *ArvB;
-
+#include<stdbool.h>
 
 int main()
 {
-      ArvB a;
+    ArvB a;
 
-      a = cria_ArvB();
+    a = cria_ArvB();
 
-      libera_ArvB(&a);
+    insere_ArvB(a,10);
+    insere_ArvB(a,12);
+    insere_ArvB(a,15);
+    insere_ArvB(a,19);
+    insere_ArvB(a,21);
 
-      ehVazia_ArvB(&a);
-
-      insere_ArvB(&a,12);
-
-      imprime_nos(&a);
-
-      libera_ArvB(&a);
-
-      imprime_nos(&a);
-
-      ehVazia_ArvB(&a);
-
-      busca_ArvB(&a,12);
-
-      printf("O numero total de nos eh de %i",totalNO_ArvB(&a));
+    imprime_nos(a);
 
 
     return 0;
