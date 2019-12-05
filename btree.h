8d4dfaf22f3e720b497ff420f49fe80b21e4e92a
Nathan Augusto <48655055+naaaathan@users.noreply.github.com>
2019-12-04 22:21:11 -0300

    Add Typedef com ponteiro

--- btree.h
+++ btree.h
@@ -1,13 +1,11 @@
+typedef struct ArvB *ArvB;
 
-
-
-
-
-ArvB* cria_ArvB() ;
-int busca_ArvB(ArvB *raiz, int valor);
-int totalNO_ArvB(ArvB *raiz);
-int ehVazia_ArvB(ArvB *raiz) ;
-int insere_ArvB(ArvB *raiz, int valor);
-int imprime_nos(ArvB *no) ;
-void libera_ArvB(ArvB *raiz);
+ArvB cria_ArvB() ;
+int busca_ArvB(ArvB raiz, int valor);
+int totalNO_ArvB(ArvB raiz);
+int ehVazia_ArvB(ArvB raiz) ;
+void insere_ArvB(ArvB raiz, int valor);
+ArvB insere_ArvB_aux(ArvB b, int valor, int *meio);
+int imprime_nos(ArvB no) ;
+int libera_ArvB(ArvB raiz);
 
